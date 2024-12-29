6b02ed9272f5d6e34b4f12d1411f122cd5dafe0e
Fergus Stonehouse <fergus.m.stonehouse@gmail.com>
2024-12-29 13:20:27 +0000

    Building Post Model

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.setdefault('0n494!z23n_@q3rfrea*7488gt0z8aj^1qf6-5p@0%vdw&_(c^')
+SECRET_KEY = os.environ.setdefault('SECRET_KEY', 'jdango-insecure-0n494!z23n_@q3rfrea*7488gt0z8aj^1qf6-5p@0%vdw&_(c^')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
