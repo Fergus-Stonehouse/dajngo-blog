8ba23e98ce3606491051158da669d80090ca976b
Fergus Stonehouse <fergus.m.stonehouse@gmail.com>
2025-01-19 14:40:07 +0000

    Add Collaboration requests to the admin page

--- about/views.py
+++ about/views.py
@@ -9,6 +9,16 @@ def about_me(request):
     Renders the 'About' site page
     """
 
+    if request.method == "POST":
+        print("Receiving a POST request here!")
+        collaborate_form = CollaboteForm(data=request.POST)
+        if collaborate_form.is_valid():
+            comment = comment_form.save(commit=False)
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
