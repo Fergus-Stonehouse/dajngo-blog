34a2c8ee9105f3dea863e012507206564dc5a7cc
Fergus Stonehouse <fergus.m.stonehouse@gmail.com>
2025-02-01 16:43:25 +0000

    Add specific tests for about form fields

--- about/test_forms.py
+++ about/test_forms.py
@@ -23,7 +23,6 @@ class TestCollaborateForm(TestCase):
         })
         self.assertFalse(form.is_valid(), msg="Name was not added but Form is valid")
 
-
     def test_form_is_valid(self):
         """ Test for email field"""
         form = CollaborateForm({
@@ -32,7 +31,6 @@ class TestCollaborateForm(TestCase):
             'message': 'Eclipse Phase for the win!'
         })
         self.assertFalse(form.is_valid(), msg="email was not added but Form is valid")
-
 
     def test_form_is_valid(self):
         """ Test for message field"""
