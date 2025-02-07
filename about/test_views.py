7f579246a1772c25e678c223321e6061b050b006
Fergus Stonehouse <fergus.m.stonehouse@gmail.com>
2025-02-07 18:17:18 +0000

    Add About Views tests

--- about/test_views.py
+++ about/test_views.py
@@ -29,3 +29,19 @@ class TestAboutViews(TestCase):
         self.assertIn(b"About Me", response.content)
         self.assertIsInstance(response.context['collaborate_form'], CollaborateForm)
     
+    def test_successful_collaboration_submission(self):
+
+        """
+        Verify the POST submission within About Me
+        """
+
+        post_data = {
+            'name': 'test name',
+            'email': 'test@email.com',
+            'message': 'test message'
+        }
+        response = self.client.post(reverse('about'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Collaboration request received! I endeavour to respond within 2 working days.',
+            response.content)
--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -19,14 +20,11 @@ def about_me(request):
     """
 
     if request.method == "POST":
-        collaborate_form = CollaboteForm(data=request.POST)
+        collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
-            comment = comment_form.save(commit=False)
-            messages.add_message(
-                request, messages.SUCCESS,
-                'Collaboration request received!'
-                'I endeavour to respond within 2 working days.'
-            )
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS,
+            "Collaboration request received! I endeavour to respond within 2 working days.")
 
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
